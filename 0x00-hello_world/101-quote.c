aa4f9df8010f58be038a6761ee7f3c889b336db9
Samuel <oharper19@gmail.com>
2023-07-14 19:25:52 -0700

    update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main -This prints the size of various types on the computer
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
