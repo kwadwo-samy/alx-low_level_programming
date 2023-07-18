9539766f481ead483558610858db608c18a063a4
Samuel <oharper19@gmail.com>
2023-07-17 18:43:07 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -This prints the size of various types on the computer
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+	a = 'a';
+	while 
+		(a <= 'z')
+		{
+			putchar (a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
