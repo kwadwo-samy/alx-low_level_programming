8d27a6dfaf78751cbf3d485666a745fe7d5911d0
Samuel <oharper19@gmail.com>
2023-07-17 19:09:15 -0700

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -This prints alphabets
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+
+	a = 'a';
+	while
+		(a <= 'z'){
+			if ((a != 'q' && a != 'e') && a <= 'z')
+				putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
